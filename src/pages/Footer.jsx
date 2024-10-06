289ca11b4bcd891ddd43a06eb52ddf9ae5980d36
rafat22 <rafat22@navgurukul.org>
2024-10-06 22:05:05 +0530

    domain name corection

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer className="w-full bg-gradient-to-r from-[#3B82F6] to-[#1E3A8A] text-white text-center py-4">
-      <p>Copyright © 2024 Printcontentprovider All Rights Reserved.</p>
+      <p>Copyright © 2024 printexpress123 All Rights Reserved.</p>
     </footer>
   );
 };
